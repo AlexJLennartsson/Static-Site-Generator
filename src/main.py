7e7381423ee6f5c266551399b75b97b05ae553d4
AlexJLennartsson <alexlennartsson@gmail.com>
2025-05-11 16:26:33 +0100

    added TextNode object

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+  dummy = TextNode("Dat some boomba blood claat eggs", TextType.NORMAL, "https://boombaclaatly.claat")
+  print(dummy)
+
+if __name__ == "__main__":
+  main()
